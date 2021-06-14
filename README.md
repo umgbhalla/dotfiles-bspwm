26659753e129d043720ec5545118383cc9b95166
Umang Bhalla <umg.bhalla88@gmail.com>
2021-06-15 00:48:26 +0530

    new repo

---
+++ README.md
@@ -0,0 +1,128 @@
+<h2 align="center"> ━━━━━━  ❖  ━━━━━━ </h2>
+
+<div align="center">
+    <code>bin</code> for scripts,
+    <code>cfg</code> for configs,
+    <code>etc</code> for other stuff
+</div>
+
+<p/>
+
+![img](./etc/ss.png)
+
+<h2 align="center"> ━━━━━━  ❖  ━━━━━━ </h2>
+
+<!--
+    Got lazy using tags lol.
+ -->
+
+### ❖ Installation
+
+   First, clone the repo.
+   ```bash
+    $ git clone --depth=1 https://github.com/umgbhalla/dotfiles-bspwm.git
+   ```
+
+   Then, update the submodules.
+   ```bash
+    $ cd dotfiles-bspwm
+   ```
+
+   Finally, run the setup script.
+   ```bash
+    $ ./setup.sh
+   ```
+
+<hr>
+
+### ❖ Keybinds
+
+
+
+  |          Keybind          |         Description         |
+  | ------------------------- | --------------------------- |
+  | super + Return                   |  # terminal alacritty												|
+  | super + shift + Return           |  # terminal kitty													|
+  | super + e                        |  # Shortcuts														|
+  | super + w                        |  # firefox														|
+  | super + n                        |  # thunar															|
+  | super + d                        |  # dmenu_run														|
+  | super + a                        |  # neovide														|
+  | super + b                        |  # bpytop															|
+  | super + r                        |  # ranger															|
+  | super + space                    |  # program launcher												|
+  | alt + Return                     |  # mini google													|
+  | alt + e                          |  # rofimoji														|
+  | alt + m                          |  # man search														|
+  | alt + r                          |  # random manpage													|
+  | alt + v                          |  # clipmenu														|
+  | alt + h                          |  # keybindhelper													|
+  | alt + p                          |  # config rofi menu ; open in nvim								|
+  | super + period                   |  # show open window												|
+  | super + shift + d                |  # show ssh sesssions												|
+  | super + p                        |  # power-menu 												|
+  | super + Escape                   |  # make sxhkd reload its configuration files:												|
+  | super + {t,shift + t,s,f}        |  # set the window state												|
+  | super + alt + {q,r}              |  # quit/restart bspwm												|
+  | super + {\_,shift + }q           |   # close and kill												|
+  | super + m                        |  # alternate between the tiled and monocle layout												|
+  | super + y                        |  # send the newest marked node to the newest preselected node												|
+  | super + g                        |  # swap the current node and the biggest window												|
+  | super + ctrl + {m,x,y,z}         |  # set the node flags												|
+  | super + {\_,shift + }{h,j,k,l}   |   # focus the node in the given direction												|
+  | super + {\_,shift + }c           |   # focus the next/previous window in the current desktop												|
+  | super + bracket{left,right}      |  # focus the next/previous desktop in the current monitor												|
+  | super + {grave,Tab}              |  # focus the last node/desktop												|
+  | super + {o,i}                    |  # focus the older or newer node in the focus history												|
+  | super + {\_,shift + }{1-8,0}     |   # focus or send to the given desktop												|
+  | super + ctrl + {h,j,k,l}         |  # preselect the direction												|
+  | super + ctrl + {1-9}             |  # preselect the ratio												|
+  | super + ctrl + space             |  # cancel the preselection for the focused node												|
+  | super + ctrl + shift + space     |  # cancel the preselection for the focused desktop												|
+  | super + alt + {h,j,k,l}          |  # expand a window by moving one of its side outward												|
+  | super + alt + shift + {h,j,k,l}  |  # contract a window by moving one of its side inward												|
+  | super + {Left,Down,Up,Right}     |  # move a floating window												|
+  | super + Print                      |  # Screenshots but better												|
+
+<hr>
+
+### ❖ Dependencies
+
+   This are all optional but you can install if you want to replicate my desktop or something.
+
+   * bspwm, sxhkd and bsp-layout
+   * rofi
+   * rofimoji
+   * polybar
+   * neovim-nightly
+   * alacritty
+   * dunst
+   * picom-ibhagwan-git
+   * light
+   * clipmenud
+   * maim, jq, xclip and viewnior
+
+<hr>
+
+### ❖ Notes
+
+   * Install NeoVim plugins
+   * i dont remember where i got this gtk theme
+
+<hr>
+
+### ❖ System Info
+
+
+   ![img](./etc/ss-1.png)
+
+<br/>
+<code>
+<hr>
+<hr>
+configs and scripts shared from https://github.com/TheRealKizu and https://github.com/siduck76
+<hr>
+fork of https://github.com/TheRealKizu/dotfiles and https://github.com/umgbhalla/dotfiles_meowV2 [i dont remember why i named it so....] 
+<hr>
+<hr>
+<code/>
---
+++ setup.sh
@@ -0,0 +1,236 @@
+#!/bin/bash
+
+title="umang's rice setup script"
+
+downloadDependencies() {
+	if grep "Arch\|Artix\|EndeavourOS\|Manjaro" /etc/*-release; then
+		clear 
+		echo "[*] Running an system update..."
+		sudo pacman --noconfirm -Syu
+
+		mkdir -p $HOME/.setup-scripto
+		sleep 0.5
+		clear
+
+		if [[ -e /usr/bin/paru ]]; then
+			echo -e "[*] paru detected. Installing dependencies..."
+			paru -S bspwm sxhkd thunar firefox dmenu clipmenu light flameshot rofi rofimoji polybar neovim-nightly-bin alacritty picom-ibhagwan-git brightnessctl playerctl dunst hsetroot maim viewnior jq xclip bsp-layout noto-fonts-emoji ttf-hack
+		elif [[ -e /usr/bin/yay ]]; then
+			echo -e "[*] yay detected. Installing dependencies..."
+			yay -S bspwm sxhkd thunar firefox dmenu clipmenu light flameshot rofi rofimoji polybar neovim-nightly-bin alacritty picom-ibhagwan-git brightnessctl playerctl dunst hsetroot maim viewnior jq xclip bsp-layout noto-fonts-emoji ttf-hack	
+			# Line from https://github.com/Axarva/dotfiles-2.0/blob/9f0a71d7b23e1213383885f2ec641da48eb01681/install-on-arch.sh#L67
+			read -r -p "Would you like to install yay? [Y/n]: " yay
+			sleep 1.5
+
+			case $yay in
+				[yY][*])
+					git clone https://aur.archlinux.org/yay.git $HOME/.setup-scripto
+					(cd $HOME/.setup-scripto && makepkg -si)
+
+					echo "[*] yay installed. Installing dependencies..."
+					yay -S bspwm sxhkd thunar firefox dmenu clipmenu light flameshot rofi rofimoji polybar neovim-nightly-bin alacritty picom-ibhagwan-git brightnessctl playerctl dunst hsetroot maim viewnior jq xclip bsp-layout noto-fonts-emoji ttf-hack						;;
+				[nN])
+					echo "[*] Okay. Will not install yay."
+					;;
+			esac 
+		fi
+
+		sleep 1
+	else
+		clear
+		echo "[*] Not on a Arch based system. Failed to download dependencies. Please manually install it."
+
+		sleep 1
+	fi
+}
+
+copyFiles() {
+	clear
+
+	sleep 1
+	echo "[*] Copying files..."
+	echo "[*] Will make backups if there are configurations found."
+
+	if [[ -d $HOME/.config/alacritty ]]; then
+		mkdir $HOME/.config/alacritty.bak && mv $HOME/.config/alacritty/* $HOME/.config/alacritty.bak
+		cp -r ./cfg/alacritty/* $HOME/.config/alacritty/
+	else
+		mkdir $HOME/.config/alacritty && cp -r ./cfg/alacritty/* $HOME/.config/alacritty
+	fi
+
+	if [[ -d $HOME/.config/bat ]]; then
+		mkdir $HOME/.config/bat.bak && mv $HOME/.config/bat/* $HOME/.config/bat.bak
+		cp -r ./cfg/bat/* $HOME/.config/bat/
+	else
+		mkdir $HOME/.config/bat && cp -r ./cfg/bat/* $HOME/.config/bat
+	fi
+
+
+	if [[ -d $HOME/.config/bspwm ]]; then
+		mkdir $HOME/.config/bspwm.bak && mv $HOME/.config/bspwm/* $HOME/.config/bspwm.bak
+		cp -r ./cfg/bspwm/* $HOME/.config/bspwm/
+	else
+		mkdir $HOME/.config/bspwm && cp -r ./cfg/bspwm/* $HOME/.config/bspwm
+	fi
+
+	if [[ -d $HOME/.config/dunst ]]; then
+		mkdir $HOME/.config/dunst.bak && mv $HOME/.config/dunst/* $HOME/.config/dunst.bak
+		cp -r ./cfg/dunst/* $HOME/.config/dunst/
+	else
+		mkdir $HOME/.config/dunst && cp -r ./cfg/dunst/* $HOME/.config/dunst
+	fi
+
+	if [[ -d $HOME/.config/htop ]]; then
+		mkdir $HOME/.config/htop.bak && mv $HOME/.config/htop/* $HOME/.config/htop.bak
+		cp -r ./cfg/htop/* $HOME/.config/htop/
+	else
+		mkdir $HOME/.config/htop && cp -r ./cfg/htop/* $HOME/.config/htop
+	fi
+
+
+	if [[ -d $HOME/.config/kitty ]]; then
+		mkdir $HOME/.config/kitty.bak && mv $HOME/.config/kitty/* $HOME/.config/kitty.bak
+		cp -r ./cfg/kitty/* $HOME/.config/kitty/
+	else
+		mkdir $HOME/.config/kitty && cp -r ./cfg/kitty/* $HOME/.config/kitty
+	fi
+
+
+	if [[ -d $HOME/.config/ncmpcpp ]]; then
+		mkdir $HOME/.config/ncmpcpp.bak && mv $HOME/.config/ncmpcpp/* $HOME/.config/ncmpcpp.bak
+		cp -r ./cfg/ncmpcpp/* $HOME/.config/ncmpcpp/
+	else
+		mkdir $HOME/.config/ncmpcpp && cp -r ./cfg/ncmpcpp/* $HOME/.config/ncmpcpp
+	fi
+
+	if [[ -d $HOME/.config/neofetch ]]; then
+		mkdir $HOME/.config/neofetch.bak && mv $HOME/.config/neofetch/* $HOME/.config/neofetch.bak
+		cp -r ./cfg/neofetch/* $HOME/.config/neofetch/
+	else
+		mkdir $HOME/.config/neofetch && cp -r ./cfg/neofetch/* $HOME/.config/neofetch
+	fi
+
+	if [[ -d $HOME/.config/nvim ]]; then
+		mkdir $HOME/.config/nvim.bak && mv $HOME/.config/nvim/* $HOME/.config/nvim.bak
+		cp -r ./cfg/nvim/* $HOME/.config/nvim/
+	else
+		mkdir $HOME/.config/nvim && cp -r ./cfg/nvim/* $HOME/.config/nvim
+	fi
+
+	if [[ -d $HOME/.config/picom ]]; then
+		mkdir $HOME/.config/picom.bak && mv $HOME/.config/picom/* $HOME/.config/picom.bak
+		cp -r ./cfg/picom/* $HOME/.config/picom/
+	else
+		mkdir $HOME/.config/picom && cp -r ./cfg/picom/* $HOME/.config/picom
+	fi
+
+	if [[ -d $HOME/.config/polybar ]]; then
+		mkdir $HOME/.config/polybar.bak && mv $HOME/.config/polybar/* $HOME/.config/polybar.bak
+		cp -r ./cfg/polybar/* $HOME/.config/polybar/
+	else
+		mkdir $HOME/.config/polybar && cp -r ./cfg/polybar/* $HOME/.config/polybar
+	fi
+
+	if [[ -d $HOME/.config/ranger ]]; then
+		mkdir $HOME/.config/ranger.bak && mv $HOME/.config/ranger/* $HOME/.config/ranger.bak
+		cp -r ./cfg/ranger/* $HOME/.config/ranger/
+	else
+		mkdir $HOME/.config/ranger && cp -r ./cfg/ranger/* $HOME/.config/ranger
+	fi
+
+	if [[ -d $HOME/.config/rofi ]]; then
+		mkdir $HOME/.config/rofi.bak && mv $HOME/.config/rofi/* $HOME/.config/rofi.bak
+		cp -r ./cfg/rofi/* $HOME/.config/rofi/
+	else
+		mkdir $HOME/.config/rofi && cp -r ./cfg/rofi/* $HOME/.config/rofi
+	fi
+
+	if [[ -d $HOME/.config/starship ]]; then
+		mkdir $HOME/.config/starship.bak && mv $HOME/.config/starship/* $HOME/.config/starship.bak
+		cp -r ./cfg/starship/* $HOME/.config/starship/
+	else
+		mkdir $HOME/.config/starship && cp -r ./cfg/starship/* $HOME/.config/starship
+	fi
+
+	if [[ -d $HOME/.config/sxhkd ]]; then
+		mkdir $HOME/.config/sxhkd.bak && mv $HOME/.config/sxhkd/* $HOME/.config/sxhkd.bak
+		cp -r ./cfg/sxhkd/* $HOME/.config/sxhkd/
+	else
+		mkdir $HOME/.config/sxhkd && cp -r ./cfg/sxhkd/* $HOME/.config/sxhkd
+	fi
+
+	if [[ -d $HOME/.config/zsh ]]; then
+		mkdir $HOME/.config/zsh.bak && mv $HOME/.config/zsh/* $HOME/.config/zsh.bak
+		cp -r ./cfg/zsh/* $HOME/.config/zsh/
+	else
+		mkdir $HOME/.config/zsh && cp -r ./cfg/zsh/* $HOME/.config/zsh
+	fi
+
+	if [[ -d $HOME/.local/bin ]]; then
+		cp -r ./bin/* $HOME/.local/bin
+	else
+		mkdir $HOME/.local/bin && cp -r ./bin/* $HOME/.local/bin
+	fi
+
+	if [ -d $HOME/.local/share/fonts ]; then
+		cp -r ./etc/fonts/* $HOME/.local/share/fonts
+	else
+		mkdir $HOME/.local/share/fonts && cp -r ./etc/fonts/* $HOME/.local/share/fonts
+	fi
+
+	sleep 1
+	echo "[*] Copied files successfully."
+	sleep 1
+}
+
+finalizeChanges() {
+	clear
+	echo "[*] Refreshing font cache..."
+	fc-cache -v
+
+	clear
+	sleep 1
+
+	clear
+	echo "[*] Finalizing changes..."
+	sleep 3
+	clear
+}
+
+fuckUser() {
+	clear
+	echo "[*] An error occured. Exiting."
+	exit
+}
+
+welcome() {
+	whiptail --title "$title" \
+		--no-button "Exit" --yes-button "Continue" \
+		--yesno "This process will download the needed dependencies and copy the config files to $HOME/.config. Would you like to continue?" 10 70
+	}
+
+success() {
+	# Remove the custom directory made by the script
+	rm -rf $HOME/.setup-scripto
+
+	whiptail --title "$title" \
+		--msgbox "Setup success. Please restart BSPWM if you are on an active session.\nCheck notes on the repository's README.\nPress alt + h on next login to see sxhkd keybinds" 20 50
+	}
+
+echo "[*] Starting setup script..."
+sleep 1
+
+# Prompt user the welcome dialog
+welcome || fuckUser
+
+# Download dependencies
+downloadDependencies
+
+# Copy files from the repo to $HOME/.config
+copyFiles
+
+# Restart everything lol
+finalizeChanges
+
+# Show the success dialog when everything is fine
+success && clear
